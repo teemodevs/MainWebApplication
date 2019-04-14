4d8076b8c4887311c0bc31ccb3da258767826c82
yechanpark <qkrdpcks0419@naver.com>
2019-04-14 11:48:42 +0900

    Initial commit

---
+++ src/main/java/teemoDevs/MainWebApplication/MainWebApplication.java
@@ -0,0 +1,13 @@
+package teemoDevs.MainWebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainWebApplication.class, args);
+	}
+
+}
