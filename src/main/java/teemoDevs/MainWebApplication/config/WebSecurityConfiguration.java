71e75afb562b6ac88ed6b4dcfc79533fba390532
yechanpark <qkrdpcks0419@naver.com>
2019-05-04 04:04:25 +0900

    resources 위치 변경 + antMatchers 수정

--- src/main/java/teemoDevs/MainWebApplication/config/WebSecurityConfiguration.java
+++ src/main/java/teemoDevs/MainWebApplication/config/WebSecurityConfiguration.java
@@ -13,7 +13,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home", "/error", "/webjars", "/js/**", "/css/**", "/img/**", "/login").permitAll()
+                .antMatchers("/", "/home", "/error", "/webjars/**", "/resources/**").permitAll()
                 .anyRequest().authenticated();
     }
 }
