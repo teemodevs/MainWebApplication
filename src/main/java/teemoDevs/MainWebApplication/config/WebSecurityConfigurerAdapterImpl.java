f2c19f15a5df610065fd5c5cd1a7adc9fe94fbf9
yechanpark <qkrdpcks0419@naver.com>
2019-05-05 18:24:58 +0900

    Optimize imports + add yml comments

--- src/main/java/teemoDevs/MainWebApplication/config/WebSecurityConfigurerAdapterImpl.java
+++ src/main/java/teemoDevs/MainWebApplication/config/WebSecurityConfigurerAdapterImpl.java
@@ -1,14 +1,8 @@
 package teemoDevs.MainWebApplication.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @Configuration
