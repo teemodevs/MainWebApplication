66b1bdf6139ee77412b81da57bea429e9908cb20
yechanpark <qkrdpcks0419@naver.com>
2019-06-15 11:21:02 +0900

    loadUser Log

--- src/main/java/teemoDevs/MainWebApplication/auth/service/CustomOAuth2UserService.java
+++ src/main/java/teemoDevs/MainWebApplication/auth/service/CustomOAuth2UserService.java
@@ -24,6 +24,17 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         // Resource Server의 /me를 호출한 결과를 바탕으로 OAuth2User를 만듦. (Default : DefaultOAuth2User)
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
+        System.out.println("OAuth2UserRequest.toString() : " + userRequest.toString());
+        System.out.println("OAuth2UserRequest.getAccessToken() : " + userRequest.getAccessToken());
+        System.out.println("OAuth2UserRequest.getAdditionalParameters() : " + userRequest.getAdditionalParameters());
+        System.out.println("OAuth2UserRequest.getClientRegistration() : " + userRequest.getClientRegistration());
+
+        System.out.println("OAuth2User.toString() : " + oAuth2User.toString());
+        System.out.println("OAuth2User.getName() : " + oAuth2User.getName());
+        System.out.println("OAuth2User.getAttributes() : " + oAuth2User.getAttributes());
+        System.out.println("OAuth2User.getAuthorities() : " + oAuth2User.getAuthorities());
+
+
         List<GrantedAuthority> grantedAuthorityList = authoritiesExtractor.extractAuthorities(oAuth2User.getAttributes());
         CustomOAuth2User customOAuth2User = new CustomOAuth2User(grantedAuthorityList, oAuth2User.getAttributes());
         customOAuth2User.setName(oAuth2User.getName());
