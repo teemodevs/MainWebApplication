7d5ecda0e5e2466953bb695951ac2da77f102616
yechanpark <qkrdpcks0419@naver.com>
2020-01-04 18:37:55 +0900

    CustomPageRequest 주석 수정

--- src/main/java/teemoDevs/MainWebApplication/web/community/model/CustomPageRequest.java
+++ src/main/java/teemoDevs/MainWebApplication/web/community/model/CustomPageRequest.java
@@ -13,26 +13,39 @@ public class CustomPageRequest {
 
     private Sort.Direction direction;
 
+    /**
+     * 쿼리할 페이지 세팅
+     */
     public CustomPageRequest setPage(int page) {
         this.page = (page <= 0) ? 1 : page;
         return this;
     }
 
+    /**
+     * 페이지 당 쿼리되는 갯수 세팅
+     */
     public CustomPageRequest setSize(int size) {
-        int DEFAULT_SIZE = 5;   // 기본 사이즈
-        int MAX_SIZE = 10;      // 최대 사이즈
-        int MIN_SIZE = 1;       // 최소 사이즈
+        int DEFAULT_SIZE = 5;   // 페이지 기본 사이즈
+        int MAX_SIZE = 10;      // 페이지 최대 사이즈
+        int MIN_SIZE = 1;       // 페이지 최소 사이즈
 
         this.size = (size > MAX_SIZE) || (size < MIN_SIZE) ? DEFAULT_SIZE : size;
         return this;
     }
 
+    /**
+     * 쿼리 시 오름차순, 내림차순 세팅
+     */
     public CustomPageRequest setDirection(Sort.Direction direction) {
         this.direction = direction;
         return this;
     }
 
+    /**
+     * 쿼리 시 페이징을 위한 객체를 리턴
+     */
     public PageRequest of() {
-        return PageRequest.of(page - 1, size, direction, "addDate");
+        String[] properties = { "addDate" }; // 정렬 시 기준이 되는 필드명 리스트
+        return PageRequest.of(page - 1, size, direction, properties);
     }
 }
